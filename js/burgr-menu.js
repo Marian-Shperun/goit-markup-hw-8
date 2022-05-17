f553e0f5f1b3da304afe76d9d184d839c71ea1e4
Marian-Shperun <marik2504911@gmail.com>
2022-05-17 19:33:36 +0200

    закриває menu при кліку на силку

--- js/burgr-menu.js
+++ js/burgr-menu.js
@@ -1,6 +1,9 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
+  const menuLink = document.querySelectorAll(
+    ".menu-container__navigation .navigation__link"
+  );
 
   menuBtnRef.addEventListener("click", () => {
     const expanded =
@@ -11,5 +14,14 @@
 
     mobileMenuRef.classList.toggle("opened");
     document.body.classList.toggle("modal-scroll");
+  });
+  
+  menuLink.forEach((el) => {
+    el.addEventListener("click", () => {
+      menuBtnRef.classList.toggle("opened");
+      mobileMenuRef.classList.toggle("opened");
+
+      document.body.classList.toggle("modal-scroll");
+    });
   });
 })();
